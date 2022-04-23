80e2eb40141784b4774b3dee4c6dfe6c386b7127
Ranieri Althoff <ranisalt@gmail.com>
2022-04-23 02:36:50 +0200

    Fix non-running script

--- ipchanger/__init__.py
+++ ipchanger/__init__.py
@@ -1,4 +1,6 @@
-from .cli import key_remap, parse_args
+__version__ = "0.1.1"
+
+from .cli import change_client_ip, key_remap, parse_args
 
 
 def run():
@@ -8,3 +10,5 @@ def run():
     for snake_case, camel_case in key_remap.items():
         if value := getattr(args, snake_case):
             url_replacements[bytes(camel_case, "ascii")] = bytes(value, "ascii")
+
+    change_client_ip(args.basedir, args.rsa, url_replacements)
--- ipchanger/cli.py
+++ ipchanger/cli.py
@@ -1,5 +1,3 @@
-__version__ = "0.1.0"
-
 import argparse
 import pathlib
 import subprocess
